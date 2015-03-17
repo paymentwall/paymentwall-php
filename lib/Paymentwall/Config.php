6dbe17be731452da67178fed11203fcc3bf3de65
Ivan Kovalyov <vanya@paymentwall.com>
2015-03-17 19:13:39 +0100

    default api - digital goods

--- lib/Paymentwall/Config.php
+++ lib/Paymentwall/Config.php
@@ -10,7 +10,7 @@ class Paymentwall_Config
 	const API_GOODS	= 2;
 	const API_CART	= 3;
 
-	protected $apiType;
+	protected $apiType = 2;
 	protected $publicKey;
 	protected $privateKey;
 	protected $apiBaseUrl = self::API_BASE_URL;
